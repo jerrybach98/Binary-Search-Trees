8500c2f7cade524f789f1f44947be14e70e82cd6
Jerry Bach <125259632+jerrybach98@users.noreply.github.com>
2023-09-24 01:07:11 -0700

    apply style guide and clean comments

--- main.rb
+++ main.rb
@@ -6,55 +6,41 @@ class Tree
     @root = build_tree(array)
   end
 
+  # Recursively builds a sorted tree
   def build_tree(array)
-    if array.length == 0
-      return nil
-    end
+    return nil if array.length == 0
 
     middle = array.length / 2
     root = Node.new(array[middle])
     root.left = build_tree(array[0...middle])
     root.right = build_tree(array[middle + 1..-1])
-    return root
+    root
   end
 
+  # Prints tree to console
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 
+  # Inserts nodes as leaves
+  def insert(node = @root, value)
+    return node = Node.new(value) if node.nil?
 
-# insert and delete method
-# will insert as a leaf
-# compare value to roots then choose order to add less/left, otherwise right
-# insert if nil (leaf)
-# don't add if duplicate?
-  def insert(node = @root, value) 
-    if node == nil
-      return node = Node.new(value)
-    end
-
-    if value < node.data 
+    if value < node.data
       node.left = insert(node.left, value)
-    elsif value > node.data 
+    elsif value > node.data
       node.right = insert(node.right, value)
     end
 
-    return node
+    node
   end
 
-  # 1st case, delete leaf easily, set it equal to nil
-  # 2nd case, replace node with child
-  # 3rd case, two children, find thing that is next biggest in array. Look far left in right subtree to replace,
-    # if the furthest left has children, it is smallest in subtree, every child will only have right subtrees
   def delete(node = @root, value)
-    # Base for recursion
-    if node == nil
-      return node
-    end
+    return node if node.nil?
 
-    # recursion for traversal
+    # Recursion for traversal
     if value < node.data
       node.left = delete(node.left, value)
       return node
@@ -63,44 +49,40 @@ class Tree
       return node
     end
 
-    # Deletion logic
-    # one or no child
-    if node.left == nil
+    # One or no child
+    if node.left.nil?
       return node.right
-    elsif node.right == nil
+    elsif node.right.nil?
       return node.left
-    # two children
+    # Two children
     else
       succ_parent = node
       successor = node.right
-      #find successor
-      until successor.left == nil
+      # Finds successor
+      until successor.left.nil?
         succ_parent = successor
         successor = successor.left
       end
 
-      # is parent of successor not same as current node? true remove successor
+      # Check if parrent of successor is not same as current node 
       if succ_parent != node
         succ_parent.left = successor.right
-      else 
+      else
         succ_parent.right = successor.right
       end
 
-      # replace node with sucessor
+      # Replace node with sucessor
       node.data = successor.data
     end
-    return node
+
+    node
   end
 
   def find(node = @root, value)
-    if node == nil
-      return false
-    end
+    return false if node.nil?
 
-    if value == node.data
-      return true
-    end
-    
+    return true if value == node.data
+
     if value < node.data
       find(node.left, value)
     elsif value > node.data
@@ -108,101 +90,78 @@ class Tree
     end
   end
 
-  # traverse and display breadth-first level order, use recursion or iteration, return an array of values
-  # use array with queue to keep track of nodes
+  # Traverse tree through level order
   def level_order(node = @root)
-    # check if empty
-    if node == nil
-      return nil
-    end
+    return nil if node.nil?
 
     queue = [node]
     output = []
     until queue.empty?
-      # pull node out of queue to process children and value
+      # Pull node out of queue to process children and value
       current = queue.shift
       output << current.data
-      # queues data
-      if current.left != nil 
-        queue << current.left
-      end
-      if current.right != nil
-        queue << current.right
-      end
+      # Queues data
+      queue << current.left unless current.left.nil?
+      queue << current.right unless current.right.nil?
     end
     output
   end
 
-  # Visit data, Left subtree, right subtree
-  # top down all the way left, once data.left nil, parents right
+  # DLR Traversal
   def preorder(node = @root)
-    if node == nil
-      return []
-    end
-    
+    return [] if node.nil?
+
     result = []
     result << node.data
-    
+
     result += preorder(node.left)
     result += preorder(node.right)
 
-    return result
+    result
   end
 
-  # go all the way down left
-  # print left, parent, right
-  def inorder (node = @root)
-    if node == nil
-      return []
-    end
+  # LDR Traversal
+  def inorder(node = @root)
+    return [] if node.nil?
 
     result = []
-    
+
     result += inorder(node.left)
     result << node.data
     result += inorder(node.right)
 
-    return result
-
+    result
   end
 
-  # left, right, data
+  # LRD Traversal
   def postorder(node = @root)
-    if node == nil
-      return []
-    end
+    return [] if node.nil?
 
     result = []
-    
+
     result += postorder(node.left)
     result += postorder(node.right)
     result << node.data
 
-    return result
+    result
   end
 
-  # find the height of the tree
+  # Height of root to leaf
   def height(node = @root, count = -1)
-    if node == nil
-      return count
-    end
+    return count if node.nil?
 
     left = height(node.left, count + 1)
     right = height(node.right, count + 1)
 
-    return [left, right].max
-
+    [left, right].max
   end
-
+  
+  # Depth of a node
   def depth(node = @root, counter = 0, value)
-    if node == nil
-      return -1
-    end
+    return -1 if node.nil?
 
-    if node.data == value 
-      return counter
-    end
-    
+    return counter if node.data == value
+
     if value < node.data
       depth(node.left, counter + 1, value)
     elsif value > node.data
@@ -210,34 +169,28 @@ class Tree
     end
   end
 
-  # use height helper function, compare sides
+  # Compare both sides with help function
   def balanced?(node = @root)
-    if node == nil 
-      return true
-    end
+    return true if node.nil?
 
     left = height(node.left)
     right = height(node.right)
 
-    if (left - right).abs > 1
-      return false
-    end
+    return false if (left - right).abs > 1
 
     balanced?(node.left) && balanced?(node.right)
   end
 
-  # grab values from tree through a traversal method, use build tree a self balancing method
-  def rebalanced()
+  def rebalanced
     @root = build_tree(level_order.sort.uniq)
-    pretty_print()
+    pretty_print
   end
-  
 end
 
-
-
+# Create node objects with left and right
 class Node
   attr_accessor :data, :left, :right
+
   include Comparable
 
   def initialize(data)
@@ -245,50 +198,26 @@ class Node
     @left = nil
     @right = nil
   end
-
 end
-
-
-# debug comments and make new tree
-# array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-# tree = Tree.new(array)
-# tree.pretty_print
-# puts "  "
-# puts "Insert new node and delete:"
-# tree.insert(2)
-# tree.delete(1)
-# tree.delete(67)
-# tree.pretty_print
-# puts tree.find(6345)
-# puts tree.find(16)
-# p "Level order: #{tree.level_order}"
-# puts "Preorder: #{tree.preorder}"
-# puts "Inorder: #{tree.inorder}"
-# puts "Postorder: #{tree.postorder}"
-# puts "Tree height: #{tree.height}"
-# puts "Node depth: #{tree.depth(8)}"
-# puts "Node depth: #{tree.depth(2)}"
-# puts tree.balanced?
-# tree.rebalanced
 
 # Test Driver Script
 array = (Array.new(15) { rand(1..100) })
 tree = Tree.new(array)
-puts "Initial tree:"
+puts 'Initial tree:'
 tree.pretty_print
 puts "Balanced: #{tree.balanced?}"
 puts "Preorder: #{tree.preorder}"
 puts "Inorder: #{tree.inorder}"
 puts "Postorder: #{tree.postorder}"
 
-puts "Insert Nodes:"
+puts 'Insert Nodes:'
 tree.insert(150)
 tree.insert(200)
 tree.insert(175)
 tree.pretty_print
 puts "Balanced: #{tree.balanced?}"
 
-puts "Rebalance:"
+puts 'Rebalance:'
 tree.rebalanced
 puts "Balanced: #{tree.balanced?}"
 puts "Preorder: #{tree.preorder}"
